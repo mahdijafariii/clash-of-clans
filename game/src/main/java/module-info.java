d0bdb7af5bdb7125b5ff61f371876e07ead8a1c6
OctavioX <126814015+OctavioX1@users.noreply.github.com>
2023-06-29 00:23:16 +0330

    Made Packages

--- game/src/main/java/module-info.java
+++ game/src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.game {
                             
     opens com.example.game to javafx.fxml;
     exports com.example.game;
+    exports com.example.ViewPackage;
+    opens com.example.ViewPackage to javafx.fxml;
 }

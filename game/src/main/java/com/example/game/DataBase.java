8a85f1f557729d748aea8a583454abc11035f404
Mahdi <mahdijm.bb@gmail.com>
2023-06-29 02:59:40 +0330

    database class is complete for test

---
+++ game/src/main/java/com/example/game/DataBase.java
@@ -0,0 +1,70 @@
+package com.example.game;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DataBase {
+    public String saveUser(String name , String password , int numberMap) throws ClassNotFoundException {
+        try {
+            String URL = "jdbc:mysql://localhost/mayagame";
+            String nameServer = "root";
+            String passwordServer = "123";
+            Class.forName("com.mysql.cj.jdbc.Driver"); // connect to database
+            Connection connection = DriverManager.getConnection(URL, nameServer, passwordServer);
+            String command = String.format("Insert INTO information (username , password , level , map , win , lose ) " +
+                    "VALUES ('%s' , '%s' , '%d', '%d', '%d', '%d')" , name,password,1,numberMap,0,0);
+            Statement statement = connection.prepareStatement(command);
+            statement.executeQuery(command);  // send info to database
+        }
+        catch (Exception e) {
+            return "Error in save User!!!";
+        }
+        return "You are saved:)";
+    }
+
+
+    public String checkUserName(String username){
+        try {
+            String URL = "jdbc:mysql://localhost/mayagame";
+            String nameServer = "root";
+            String passwordServer = "123";
+            Class.forName("com.mysql.cj.jdbc.Driver"); // connect to database
+            Connection connection = DriverManager.getConnection(URL, nameServer, passwordServer);
+            String sql = "SELECT username FROM `information`;";
+            Statement s = connection.prepareStatement(sql);
+            ResultSet rs = s.executeQuery(sql);
+            while (rs.next()){
+                if(rs.equals(username)){
+                    return "We have this username in our game !! ";
+                }
+            }
+        }
+        catch (Exception e){
+
+        }
+        return "checked";
+    }
+
+
+
+    public String updateLevel(String username , int level){
+        try {
+            String URL = "jdbc:mysql://localhost/mayagame";
+            String nameServer = "root";
+            String passwordServer = "123";
+            Class.forName("com.mysql.cj.jdbc.Driver"); // connect to database
+            Connection connection = DriverManager.getConnection(URL, nameServer, passwordServer);
+            String sql =String.format( "UPDATE `information` SET level='%d' WHERE username='%s'" , level ,username );
+            Statement s = connection.prepareStatement(sql);
+            s.executeQuery(sql);
+        }
+        catch (Exception e){
+            return "we could not update the level ! ";
+
+        }
+        return "updated!";
+    }
+
+}

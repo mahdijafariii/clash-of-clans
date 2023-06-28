dd52ab35b40e61bf87fec1025cfffa8156dcef01
Mahdi <mahdijm.bb@gmail.com>
2023-06-29 03:10:38 +0330

    complete the crate acc page

--- game/src/main/java/com/example/ViewPackage/CreateAccController.java
+++ game/src/main/java/com/example/ViewPackage/CreateAccController.java
@@ -1,7 +1,9 @@
 package com.example.ViewPackage;
 
+import com.example.game.DataBase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
@@ -28,7 +30,18 @@ public class CreateAccController {
     private TextField userName;
 
     @FXML
-    void confirmSign(ActionEvent event) {
-
+    void confirmSign(ActionEvent event) throws ClassNotFoundException {
+        DataBase dataBase = new DataBase();
+        String check = dataBase.checkUserName(userName.getText());
+        if (check.equals("checked")){
+            dataBase.saveUser(userName.getText(),password.getText(),Integer.valueOf(numberMap.getText()));
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Unacceptable name!!");
+            alert.setHeaderText(null);
+            alert.setContentText("This name is already selected!! Please choose another name!");
+            alert.show();
+        }
     }
 }
--- game/src/main/java/com/example/ViewPackage/HelloController.java
+++ game/src/main/java/com/example/ViewPackage/HelloController.java
@@ -2,9 +2,17 @@ package com.example.ViewPackage;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class HelloController {
 
@@ -19,13 +27,18 @@ public class HelloController {
     private TextField userNameInput;
 
     @FXML
-    void createAccount(ActionEvent event) {
+    void createAccount(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("create-acc-controller.fxml")));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) (event.getSource())).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Create Account");
+        stage.show();
 
     }
 
     @FXML
     void login(ActionEvent event) {
-
     }
 
 

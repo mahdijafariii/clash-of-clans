0801a713d7bb73a753bb526b636f1ed14fd654d7
Mahdi <mahdijm.bb@gmail.com>
2023-06-28 23:48:39 +0330

    create sign and login page

---
+++ game/src/main/java/com/example/game/CreateAccController.java
@@ -0,0 +1,34 @@
+package com.example.game;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class CreateAccController {
+    @FXML
+    private ImageView map1;
+
+    @FXML
+    private ImageView map2;
+
+    @FXML
+    private ImageView map3;
+
+    @FXML
+    private ImageView map4;
+
+    @FXML
+    private TextField numberMap;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    void confirmSign(ActionEvent event) {
+
+    }
+}
---
+++ game/src/main/java/com/example/game/HelloController.java
@@ -0,0 +1,32 @@
+package com.example.game;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class HelloController {
+
+
+    @FXML
+    private TextField passwordInput;
+
+    @FXML
+    private ImageView signImage;
+
+    @FXML
+    private TextField userNameInput;
+
+    @FXML
+    void createAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+
+}

0d124ae6acc35d73e3b006b66d8fbe2d210fcc84
Mahdi <mahdijm.bb@gmail.com>
2023-07-03 22:07:48 +0330

    start attack of heroes

--- game/src/main/java/com/example/HeroPackage/elves/FirstElf.java
+++ game/src/main/java/com/example/HeroPackage/elves/FirstElf.java
@@ -1,6 +1,11 @@
 package com.example.HeroPackage.elves;
 
+import com.example.BuildingPackage.Building;
+import com.example.BuildingPackage.Towers.CastleBridgeTower;
+import com.example.BuildingPackage.Towers.JungleTower;
+import com.example.BuildingPackage.Towers.SkyBridgeTower;
 import com.example.HeroPackage.Heroes;
+import com.example.UserPackage.Administrator;
 import javafx.scene.image.Image;
 
 public class FirstElf extends Heroes {
@@ -62,6 +67,61 @@ public class FirstElf extends Heroes {
         else {
             setImage(new Image(img_walk2));
         }
+
+    }
+
+    //--------------------------------------------------- Walk x , y
+
+    public void walkForAttackX(){
+        action++;
+        setTranslateX(getTranslateX()+speed);
+        if(action%2==0){
+            setImage(new Image(img_walk1));
+        }
+        else {
+            setImage(new Image(img_walk2));
+        }
+    }
+    public void walkForAttackY(){
+        action++;
+        setTranslateY(getTranslateY()+speed);
+        if(action%2==0){
+            setImage(new Image(img_walk1));
+        }
+        else {
+            setImage(new Image(img_walk2));
+        }
+    }
+    //--------------------------------------------------- Walk x , y
+
+    //--------------------------------------------------- Attack and change attack picture
+
+    public void attackInBattle(){
+        action++;
+        setTranslateY(getTranslateY()+1);
+        setTranslateY(getTranslateY()-1);
+        if(action%3==0){
+            setImage(new Image(img_Attack1));
+        }
+        else if (action%3==1){
+            setImage(new Image(img_Attack2));
+
+        }
+        else {
+            setImage(new Image(img_Attack3));
+        }
+    }
+    //--------------------------------------------------- Attack and change attack picture
+
+
+    public void StartBattleAttacking(){
+
+        for(int i = 0 ; i <Administrator.getCurrentMap().getBuildings().size() ; i++ ){
+            if(Administrator.getCurrentMap().getBuildings().get(i) instanceof CastleBridgeTower || Administrator.getCurrentMap().getBuildings().get(i) instanceof JungleTower ||Administrator.getCurrentMap().getBuildings().get(i) instanceof SkyBridgeTower){
+
+            }
+        }
+
 
     }
 
